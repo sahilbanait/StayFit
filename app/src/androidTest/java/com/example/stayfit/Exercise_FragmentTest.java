cc96e3c8549d37a03c03720d4944679d73f5837d
sahilbanait <77091616+sahilbanait@users.noreply.github.com>
2021-09-28 13:55:43 +0800

    Updated Test methods

--- app/src/androidTest/java/com/example/stayfit/Exercise_FragmentTest.java
+++ app/src/androidTest/java/com/example/stayfit/Exercise_FragmentTest.java
@@ -7,21 +7,31 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.assertThat;
 import static androidx.test.espresso.matcher.ViewMatchers.hasImeAction;
+import static androidx.test.espresso.matcher.ViewMatchers.hasTextColor;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import android.app.Instrumentation;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.IdRes;
 import androidx.fragment.app.testing.FragmentScenario;
+import androidx.lifecycle.Lifecycle;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.testing.TestNavHostController;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.ViewAction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
 
 import org.hamcrest.Matcher;
 import org.junit.After;
@@ -31,6 +41,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class Exercise_FragmentTest {
 
     @Rule
@@ -41,27 +52,41 @@ public class Exercise_FragmentTest {
     public void setUp() throws Exception {
     }
 
+//  Email validation testing
     @Test
     public void emailValidatorTest() {
-
-        onView(withId(R.id.text_email)).perform(typeText("sahil.saini526@gmail.com"));
-        onView(withId(R.id.proceed_button)).perform(click()).check(matches(isDisplayed()));
+        onView(withId(R.id.text_email)).perform(typeText("xyz@gmail.com")).check(matches(isDisplayed()));
+//
 
     }
     @Test
+    public void floatingButtonTest(){
+        onView(withId(R.id.floating_button)).perform(click()).check(matches(isDisplayed()));
+
+    }
+
+//    Up navigation testing
+    @Test
     public void testUpNavigation(){
-        TestNavHostController navHostController = new TestNavHostController( ApplicationProvider.getApplicationContext());
-        FragmentScenario<Exercise_Fragment> scenario = FragmentScenario.launch(Exercise_Fragment.class);
-        scenario.onFragment( exercise_fragment -> {
-             navHostController.setGraph(R.navigation.navigation);
-             Navigation.setViewNavController(exercise_fragment.requireView(), navHostController);
-                });
+        @IdRes final int theme = R.style.Theme_AppCompat_DayNight;
 
-        onView(withId(R.id.proceed_button)).perform(click());
+        Bundle args = new Bundle();
+        FragmentScenario<Exercise_Fragment> navhostScenario = FragmentScenario.launchInContainer(Exercise_Fragment.class, args, theme, Lifecycle.State.STARTED);
 
+        navhostScenario.onFragment(fragment -> {
 
+            // Create a NavController and set the NavController property on the fragment
+            assertNotNull(fragment.getActivity());
+            TestNavHostController navController = new TestNavHostController(fragment.getActivity());
+            fragment.getActivity().runOnUiThread(() -> navController.setGraph(R.navigation.navigation));
+            Navigation.setViewNavController(fragment.requireView(), navController);
 
-
+            // Then navigate
+            navController.navigate(R.id.action_title_to_exercise_Fragment);
+            NavDestination destination = navController.getCurrentDestination();
+            assertNotNull(destination);
+            assertEquals(destination.getId(), R.id.exercise_Fragment);
+        });
     }
 
     @After
